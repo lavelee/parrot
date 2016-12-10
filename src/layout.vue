b94c94b3f11d205986b231db9756fe9538308cd4
lavelee <lavelee15@gmail.com>
2016-12-10 21:04:29 +0900

    vue component 분리, webpack에서 불러와지는거 확인. 문제점 : 쓸데없이 너무 파일이 많이 분할됨.

--- src/layout.vue
+++ src/layout.vue
@@ -5,12 +5,12 @@
 </template>
 
 <script>
-import logInDim from './components/app_partial/login-dim.vue'
+import logInDim from './components/app_partial/loginDim.vue'
 import logIn from './components/app_partial/login.vue'
-import contentsDetail from './components/app_partial/contents_detail/contents_detail.vue'
+import contentsDetail from './components/app_partial/contents_detail/contentsDetail.vue'
 
 export default {
-  name: 'layout'
+  name: 'layout',
   components: {
     logInDim, logIn, contentsDetail
   }

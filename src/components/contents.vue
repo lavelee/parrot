a6f26074a5316ab2fd57333faab00d0cf83149ff
lavelee <lavelee15@gmail.com>
2016-12-20 22:23:11 +0900

    star, contets disappearing bug fixed : because of weard chrome rendering pattern.

--- src/components/contents.vue
+++ src/components/contents.vue
@@ -3,55 +3,29 @@
   <!-- 본문 카드타일 래퍼 -->
   <!-- <main v-on:scroll="scrollFunction"> -->
   <main>
-  <div v-for="page,index in contents_list" class="page">
-  <ul class="card-frame">
-    <!-- v-for 로 여러개 생성할 부분 -->
-    <li v-for="contents in page" class="card-single"><a href="">
-      <img v-bind:src=contents.img_thumbnail alt="background">
-      <div class="card-texts">
-      <p class="card-hashtags">
-        <span class="hashtag-single">333</span>
-      </p>
-      <p class="card-short-content">{{contents.content}}</p>
-      <p class="card-date">{{contents.created_date.slice(0,16).replace("T"," ")}}</p>
-      <p class="card-distance">{{contents.distance}}</p>
-      <p class="card-stars">{{contents.like_users_counts}}</p>
-      <p class="card-replies">{{contents.comments_counts}}</p>
-    </div>
+    <div v-for="page,index in contents_list"class="page">
+      <!-- v-for 로 여러개 생성할 부분 -->
+    <ul class="card-frame">
+      <li v-for="contents in page" class="card-single"><a href="">
+        <img v-bind:src=contents.img_thumbnail alt="background">
+        <div class="card-texts">
+          <p class="card-hashtags">
+            <span class="hashtag-single">333</span>
+          </p>
+          <p class="card-short-content">{{contents.content}}</p>
+          <p class="card-date">{{contents.created_date.slice(0,16).replace("T"," ")}}</p>
+          <p class="card-distance">{{contents.distance}}</p>
+          <p class="card-stars">{{contents.like_users_counts}}</p>
+          <p class="card-replies">{{contents.comments_counts}}</p>
+      </div>
     </a></li>
-
   </ul>
-
-<div class="page-splitter">
-  <span>{{(index)*n_post_initial_page+page.length}}</span>
-  <span v-if="last_post_notice&&(index===(contents_list.length-1))">end of post</span>
-</div>
-</div>
-
-<!-- <ul class="card-frame">
-<li class="card-single"><a href="">
-  <img src="http://placehold.it/728x490" alt="background">
-  <div class="card-texts">
-  <p class="card-hashtags">
-    <span class="hashtag-single">333</span>
-  </p>
-  <p class="card-short-content">1-1 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-  <p class="card-date">2016-12-12</p>
-  <p class="card-distance">12km</p>
-  <p class="card-stars">5</p>
-  <p class="card-replies">11</p>
-</div>
-</a></li>
-</ul>
-<div class="page-splitter">
-<span>10</span>
-</div> -->
-
-
-
-
-
-
+      <div class="page-splitter">
+        <span>{{(index)*n_post_initial_page+page.length}}</span>
+        <span v-if="last_post_notice&&(index===(contents_list.length-1))">end of post</span>
+      </div>
+    </div>
+    <!-- </transition-group> -->
   </main>
 
   <!-- 상세보기시 뜨는 창 구성 -->
@@ -82,14 +56,11 @@
   </div>
   </section>
   </div>
-
-
 </template>
 
 
 
 <script>
-
 import parrot from '../config.js'
 export default {
   name: 'Contents',
@@ -105,32 +76,18 @@ export default {
   components: {   },
   methods: {
     addPostData(address,initial=false){
-      // console.log("api address:",address)
-      // console.log("this: ",this)
-      // console.log(this.api_post)
-      // console.log("initial: ",initial)
       this.axios.get(address).then((response) => {
         this.next_postpage_link = response.data.next
         if(!this.next_postpage_link){
           this.last_post_notice=true
-          console.log("last post notice : ",this.last_post_notice)
         }
-        console.log("test",this.last_post_notice&&(3===this.contents_list.length))
-        // console.log("this.next_postpage_link updated :",this.next_postpage_link)
         this.contents_list = this.contents_list.concat([response.data.results])
-        // console.log("parent can_retrive before responding",this.$parent.can_retrive)
         if (initial){
           this.n_post_initial_page = this.contents_list[0].length
         }
         if(!initial){
-          // this.$parent.can_retrive=true
           window.eventbus.$emit('addpost_api_callfinished')
-          // console.log("parent can_retrive modification",this.$parent.can_retrive)
-          // console.log("this.n_post_initial_page",this.n_post_initial_page)
-          // console.log("!address",!address)
-
         }
-        // console.log("this.contents_list",this.contents_list)
       })
     }
   },
@@ -139,18 +96,11 @@ export default {
     this.api_post = parrot.server_dir+'/post/'
     this.addPostData(this.api_post,true)
     window.eventbus.$on('scroll',function(){
-      // console.log("this:",this)
       self.addPostData(self.next_postpage_link)
     })
   }
 }
-
-
-
-
-
 </script>
-
 
 
 <style lang='sass'>
